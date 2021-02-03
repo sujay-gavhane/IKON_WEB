6454b3d69d1166e5ece50f8dff1e2ee3f605051f
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-03 17:38:22 +0530

    Added static pages model

--- db/schema.rb
+++ db/schema.rb
@@ -46,4 +46,11 @@ ActiveRecord::Schema.define(version: 2021_02_03_113510) do
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
   end
 
+  create_table "static_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
