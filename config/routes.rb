b5ed18914434792a7924a6c5774650a89030f44e
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-03 17:59:35 +0530

    Added Home Page for customer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/ikon_admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root to: '/ikon_admin'
+  root to: '/', controller: 'home', action: 'index'
 end
