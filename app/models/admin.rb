d3da098e46f20f16b223cebf429419956dad1513
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-03 17:36:35 +0530

    Added validations on admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -4,4 +4,8 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 end
