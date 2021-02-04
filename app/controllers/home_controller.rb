b8a901a0714ea65dbbb2b936d339e4d3063a5f24
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-04 13:21:45 +0530

    Added static pages on customer side

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,20 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about_us
+    @about_us = StaticPage.find_by(name: 'About Us').content.html_safe
+  end
+
+  def privacy_policy
+    @privacy_policy = StaticPage.find_by(name: 'Privacy Policy').content.html_safe
+  end
+
+  def terms_and_conditions
+    @terms_and_conditions = StaticPage.find_by(name: 'Terms and Conditions').content.html_safe
+  end
+
+  def faq
+    @faq = StaticPage.find_by(name: 'faq').content.html_safe
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/ikon_admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: '/', controller: 'home', action: 'index'
+  get '/about_us', to: 'home#about_us'
+  get '/privacy_policy', to: 'home#privacy_policy'
+  get '/terms_and_conditions', to: 'home#terms_and_conditions'
+  get '/faq', to: 'home#faq'
 end
