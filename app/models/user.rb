5933b6a15bf23c738dbdda9d1cbd639bc96bc223
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-05 18:12:20 +0530

    Added validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  validates :email, presence: true
+
 end
