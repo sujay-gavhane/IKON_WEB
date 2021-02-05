c455043c99911be71b3091d17b13e80b0b20e248
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-05 18:12:52 +0530

    Refactored

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,21 +3,31 @@ class HomeController < ApplicationController
   end
 
   def about_us
-    @about_us = StaticPage.find_by(name: 'About Us').content.html_safe
+    @about_us = StaticPage.find_by(name: 'About Us')
+    @about_us = get_content(@about_us)
   end
 
   def privacy_policy
-    @privacy_policy = StaticPage.find_by(name: 'Privacy Policy').content.html_safe
+    @privacy_policy = StaticPage.find_by(name: 'Privacy Policy')
+    @privacy_policy = get_content(@privacy_policy)
   end
 
   def terms_and_conditions
-    @terms_and_conditions = StaticPage.find_by(name: 'Terms and Conditions').content.html_safe
+    @terms_and_conditions = StaticPage.find_by(name: 'Terms and Conditions')
+    @terms_and_conditions = get_content(@terms_and_conditions)
   end
 
   def faq
-    @faq = StaticPage.find_by(name: 'faq').content.html_safe
+    @faq = StaticPage.find_by(name: 'faq')
+    @faq = get_content(@faq)
   end
 
   def contact_us
+  end
+
+  private
+
+  def get_content(page)
+    page.content.html_safe if page.present?
   end
 end
