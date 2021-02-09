017bfd23377349319a2092de874d126347d8098a
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-09 21:19:45 +0530

    collection type schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_083250) do
+ActiveRecord::Schema.define(version: 2021_02_09_114922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,12 @@ ActiveRecord::Schema.define(version: 2021_02_05_083250) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "collection_types", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -52,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_02_05_083250) do
     t.bigint "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "collection_type_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
