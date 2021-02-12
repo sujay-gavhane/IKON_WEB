64c6915e449f26212dff43ff516467d0ed80dcfb
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-02-12 23:46:54 +0530

    removed null: false validation from collection_type_id column

--- db/migrate/20210209114922_add_collection_type_id_to_products.rb
+++ db/migrate/20210209114922_add_collection_type_id_to_products.rb
@@ -1,5 +1,5 @@
 class AddCollectionTypeIdToProducts < ActiveRecord::Migration[6.0]
   def change
-    add_column :products, :collection_type_id, :integer, null: false
+    add_column :products, :collection_type_id, :integer
   end
 end
