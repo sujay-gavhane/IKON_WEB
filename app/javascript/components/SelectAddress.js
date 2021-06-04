23073718269181bd5441a2d729128ce2ca45de0e
Sujay Gavhane <sujay.gavhane@gmail.com>
2021-06-04 17:54:23 +0530

    Added add new address feature on checkout page

--- app/javascript/components/SelectAddress.js
+++ app/javascript/components/SelectAddress.js
@@ -1,6 +1,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import axios from 'axios';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import NewAddress from "./NewAddress"
+
 class SelectAddress extends React.Component {
   state = {
     addresses: []
@@ -10,10 +14,16 @@ class SelectAddress extends React.Component {
 
     this.getAddressItems = this.getAddressItems.bind(this);
     this.setAddress = this.setAddress.bind(this);
+    this.updateStates = this.updateStates.bind(this);
+    this.open = this.open.bind(this);
   }
 
   componentDidMount() {
     this.getAddressItems();
+  }
+
+  open(){
+    this.updateStates('show', 'open')
   }
 
   getAddressItems = () => {
@@ -27,6 +37,10 @@ class SelectAddress extends React.Component {
            return null;
        });
   };
+
+  updateStates(name, val){
+    this.setState({ [name]: val })
+  }
 
   setAddress() {
     this.props.selectedAddress(event.target.closest("a").dataset.address)
@@ -47,8 +61,13 @@ class SelectAddress extends React.Component {
       <React.Fragment>
         <main className={"popup-main choose-address " + this.props.toggleAddressPopup}>
           <div className="popup-div">
-            <a onClick={this.props.closePopup} className="close-btn"> <span></span> </a>
-            <div className="my-address">
+            <div className='select-address'>
+              <h2 style={{display: 'inline'}}>Select Address</h2>
+              <button onClick={this.open} type="button" name="button"><FontAwesomeIcon icon={faPlus} /></button>
+              <a onClick={this.props.closePopup} className="close-btn"> <span></span> </a>
+            </div>
+            <NewAddress updateAddressList={this.getAddressItems} setPopupProps={this.updateStates} show={this.state.show} />
+            <div className="my-address" style={{maxHeight: '500px', overflowY: 'scroll'}}>
               {addresses}
             </div>
           </div>
